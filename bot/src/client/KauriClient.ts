f9f122eb869fea33c9d1a64eb3ea3246e829138d
monbrey <monbrey@gmail.com>
2022-10-16 15:46:28 +1100

    feat: command and event handlers

---
+++ bot/src/client/KauriClient.ts
@@ -0,0 +1,21 @@
+import { Client, ClientOptions } from 'discord.js';
+import { join } from 'node:path';
+import { CommandHandler } from '../handlers/CommandHandler.js';
+import { EventHandler } from '../handlers/EventHandler.js';
+
+export class KauriClient extends Client {
+	public slashCommands: CommandHandler;
+	public events: EventHandler;
+
+	public constructor(options: ClientOptions) {
+		super(options);
+
+		this.slashCommands = new CommandHandler(this, {
+			directory: join(__dirname, 'commands', '')
+		}).loadAll().setup();
+
+		this.events = new EventHandler(this, {
+			directory: join(__dirname, 'events')
+		}).loadAll();
+	}
+}
