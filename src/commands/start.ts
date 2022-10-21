77c731d18cf2fbea877f353fc0248bf7eac43b4b
monbrey <monbrey@gmail.com>
2022-10-21 12:31:56 +1100

    feat: autocomplete starter-eligible pokemon

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,7 +1,15 @@
 import type { AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js";
 import { SlashCommandBuilder } from "discord.js";
 import { request } from "undici";
-import { findBestMatch } from "string-similarity";
+import { findBestMatch, Rating } from "string-similarity";
+
+const cache: {
+	data: string[];
+	lastFetched: number;
+} = {
+	data: [],
+	lastFetched: 0,
+};
 
 export const data = new SlashCommandBuilder()
 	.setName("start")
@@ -12,11 +20,16 @@ export const data = new SlashCommandBuilder()
 		.setAutocomplete(true));
 
 export const autocomplete = async (interaction: AutocompleteInteraction<"cached">) => {
-	const { data } = await request("https://kauri.monbrey.com.au/items/species").then(async ({ body }) => body.json());
-	const names = data.map((d: any) => d.name);
-	const { ratings } = findBestMatch(interaction.options.getFocused(), names);
+	if (cache.lastFetched < Date.now() - 86400) {
+		const { data } = await request("https://kauri.monbrey.com.au/items/species", {
+			query: { limit: 1000 },
+		}).then(async ({ body }) => body.json());
+		cache.data = data.filter((d: any) => d.starter_eligible).map((d: any) => d.name);
+	}
 
+	const { ratings } = findBestMatch(interaction.options.getFocused(), cache.data);
 	const response = ratings.sort((a, b) => b.rating - a.rating).map(r => ({ name: r.target, value: r.target })).slice(0, 5);
+	console.log(response);
 	void interaction.respond(response);
 };
 
