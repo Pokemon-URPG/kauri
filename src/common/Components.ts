dd089da054739a1c6e63237eba66932cadb12dde
monbrey <monbrey@gmail.com>
2022-10-25 18:38:13 +1100

    fix: specify return type of component methods

--- src/common/Components.ts
+++ src/common/Components.ts
@@ -1,6 +1,10 @@
+import type { ButtonComponentData } from "discord.js";
 import { ButtonStyle, ComponentType } from "discord.js";
 
+/**
+ * Reusable standard buttons
+ */
 export const Buttons = {
-	Confirm: (id?: string) => ({ type: ComponentType.Button, customId: id ?? "confirm", label: "Confirm", style: ButtonStyle.Primary }),
-	Cancel: (id?: string) => ({ type: ComponentType.Button, customId: id ?? "cancel", label: "Cancel", style: ButtonStyle.Secondary }),
+	Confirm: (id?: string): ButtonComponentData => ({ type: ComponentType.Button, customId: id ?? "confirm", label: "Confirm", style: ButtonStyle.Primary }),
+	Cancel: (id?: string): ButtonComponentData => ({ type: ComponentType.Button, customId: id ?? "cancel", label: "Cancel", style: ButtonStyle.Secondary }),
 };
